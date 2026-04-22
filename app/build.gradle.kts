941da1542c0b166e331bb6d7c04d8b4d4029c6b7
irankunda-steve <irankundasteve22@gmail.com>
2026-04-23 01:29:03 +0200

    Fix memory limits and disable lint aborting on build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,11 @@ android {
         }
     }
 
+    lint {
+        checkReleaseBuilds = false
+        abortOnError = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
