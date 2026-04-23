bf3b1a5e99852f0a93815c0f2a16484722bbf240
irankunda-steve <irankundasteve22@gmail.com>
2026-04-23 04:06:41 +0200

    Update compileSdk and targetSdk to 34 for Room compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.helloworld"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.helloworld"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
