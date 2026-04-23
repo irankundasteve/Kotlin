e206b9a65301ea4edefb7efd351e5ff6860e3b5a
irankunda-steve <irankundasteve22@gmail.com>
2026-04-23 03:21:04 +0200

    Fix build by adding lifecycle and coroutine dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ android {
 
 dependencies {
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
